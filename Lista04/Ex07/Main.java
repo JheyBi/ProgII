3f18942cdb74a55fb6926d831b65b3593c03cb3c
João Bernardo Del Rio <54190405+JheyBi@users.noreply.github.com>
2022-07-15 11:26:45 -0300

    Create Main.java

---
+++ Lista04/Ex07/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();
+        
+        System.out.println(jogo.colidiu());
+    }
+    
+}
